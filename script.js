10d329d714c4c9b9d2f0f6b105b5de3ce377732f
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-01-21 13:31:39 +0000

    Remove snake tail when direction change!

--- script.js
+++ script.js
@@ -36,12 +36,13 @@ function gamePlay() {
   player.start = true;
   drawSnake()
   createFood()
-  setInterval(moveSnake, 2000)
+  setInterval(moveSnake, 57)
   // drawFood()
   // moveSnake()
   // window.webkitRequestAnimationFrame(gamePlay)
 }
 document.addEventListener('keydown', changeDirection)
+
 
 function createFood() {
   ctx.fillStyle = 'red'
@@ -87,18 +88,23 @@ function gameOver() {
 
 // Change Direction
 function changeDirection(event) {
+  console.log(snake.length)
   // up
   if (event.keyCode == 38) {
     if(yDirection == tileSize)
       return;
+      const tail = snake[snake.length-1];
+    ctx.clearRect(tail.x, tail.y, tileSize, tileSize)
     yDirection= - tileSize; //move one tile up
+    // console.log(tail.x-(tileSize*2), tail.y-(tileSize*2))
     xDirection = 0;
-
   }
   //down
   if (event.keyCode == 40) {
     if(yDirection == -tileSize)
       return;
+      const tail = snake[snake.length-1];
+    ctx.clearRect(tail.x, tail.y, tileSize, tileSize)
     yDirection= tileSize ;//move one tile down
     xDirection = 0;
   }
@@ -106,15 +112,19 @@ function changeDirection(event) {
   if (event.keyCode == 37) {
     if(xDirection == tileSize)
       return;
-    yDirection = 0;
+      const tail = snake[snake.length-1];
+    ctx.clearRect(tail.x, tail.y, tileSize, tileSize)
     xDirection = -tileSize;//move one tile left
+    yDirection = 0;
   }
   //right
   if (event.keyCode == 39) {
     if(xDirection == -tileSize)
       return;
-    yDirection= 0 ;
+      const tail = snake[snake.length-1];
+    ctx.clearRect(tail.x, tail.y, tileSize, tileSize)
     xDirection = tileSize;//move one tile right
+    yDirection= 0 ;
   }
 
 changeSnakePosition()
