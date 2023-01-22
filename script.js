23e063a494218d3005db3e5d020c09ce8cc3435f
Falak Fatma <102820083+falakfatma@users.noreply.github.com>
2023-01-22 18:01:24 +0000

    Alert when snake eat food and alert when you out.

--- script.js
+++ script.js
@@ -12,16 +12,18 @@ let keys = {
   ArrowDown: 'ArrowDown',
   ArrowRight: 'ArrowRight'
 }
+let foodX;
+let foodY;
 
-let appleX = Math.floor(Math.random() * (canvas.width - tileSize))
-let appleY = Math.floor(Math.random() * (canvas.height - 100))
+// let foodX = Math.floor(Math.random() * (canvas.width - tileSize))
+// let foodY = Math.floor(Math.random() * (canvas.height - 100))
 let snake = [
   {
     x: tileSize,
     y: 10,
   },
 ]
-
+generateFood()
 gameAlert.addEventListener('click', () => {
   gameAlert.classList.add('hide');
   startGame();
@@ -34,20 +36,32 @@ function startGame() {
 
 function gamePlay() {
   console.log(player.start)
-  if(player.start){
-  drawSnake()
-  createFood()
-  setInterval(gameOver, 100)
-  setInterval(moveSnake, 100)
+  if (player.start) {
+    drawSnake()
+    setInterval(createFood, 100)
+    setInterval(gameOver, 100)
+    setInterval(moveSnake, 100)
   }
 }
 document.addEventListener('keydown', changeDirection)
-
-
 function createFood() {
+
   ctx.fillStyle = 'red'
-  ctx.fillRect(appleX, appleY, tileSize, tileSize)
+  ctx.fillRect(foodX, foodY, tileSize, tileSize)
 }
+function random_food(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+function generateFood() {
+  foodX = random_food(0, canvas.width - tileSize);
+  foodY = random_food(0, canvas.height - tileSize);
+  snake.forEach(function hasSnakeEatenFood(part) {
+    const has_eaten = part.x == foodX && part.y == foodY;
+    if (has_eaten) generateFood();
+  });
+}
+
 
 function moveSnake() {
   const snakeHead = {
@@ -57,8 +71,10 @@ function moveSnake() {
   // console.log('before unshift', snake.length)
   snake.unshift(snakeHead)
   // console.log(snake.length)
-  if (snake[0].x == appleX && snake[0].y == appleY) {
-    console.log(`you eat apple`)
+  if (snake[0].x == foodX && snake[0].y == foodY) {
+    alert(`you eat apple`)
+    createFood()
+
   }
   else {
     const tail = snake[snake.length - 1];
@@ -77,12 +93,29 @@ function drawSnake() {
 
 
 function gameOver() {
-  if (snake[0].x == 0 || snake[0].x == canvas.width - tileSize || snake[0].y == 0 || snake[0].y == canvas.width - tileSize) {
-    console.log(`You Are Out!`)
-   //  player.start = false
-   //  console.log(player.start)
-   // gameAlert.classList.remove('hide')
+  let topOut = snake[0].y <= 0;
+  let leftOut = snake[0].x <= 0 - tileSize;
+  let bottomOut = snake[0].y >= canvas.height - tileSize;
+  let rightOut = snake[0].x >= canvas.width - tileSize;
+  for (let i = 1; i < snake.length; i++) {
+    let collide = snake[0].x == snake[i].x && snake[0].y == snake[i].y
+    if (collide) {
+      console.log(`game over`)
+    }
   }
+  if (topOut || leftOut || bottomOut || rightOut) {
+    console.log(`You are out `)
+  } else {
+    console.log(`wow nice Played`)
+  }
+  function clearScreen() {
+
+  }
+  // console.log("You Out from left side : "+ leftOut.valueOf())
+  // console.log("You Out from top side : "+ topOut)
+  // console.log("You Out from bottom side : "+ bottomOut)
+  // console.log("You Out from right side : "+ rightOut)
+  // if(leftOut)
 }
 
 // Change Direction
